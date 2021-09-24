1dc358a68c2ada5a3705d699344f12e91bf106d8
Ivanop1 <ipaniagua874@gmail.com>
2021-09-24 12:51:32 -0300

    Primer commit, maqueta de la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { LoginComponent } from './login/login/login.component';
+import { NavbarComponent } from './compartida/navbar/navbar.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { ProductosComponent } from './productos/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
